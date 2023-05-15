81a84ffd3648afb75a1ba5229a0c9dd5416a015c
Bojana Bogicevic <92739976+BogicevicBojana@users.noreply.github.com>
2023-05-15 23:34:12 +0200

    Add config to program.cs and add nuget packages

--- backend/CompanyManagementSystem.API/Program.cs
+++ backend/CompanyManagementSystem.API/Program.cs
@@ -1,5 +1,15 @@
-using Microsoft.EntityFrameworkCore;
 using CompanyManagementSystem.Data;
+using CompanyManagementSystem.DataAccess;
+using Microsoft.EntityFrameworkCore;
+using Newtonsoft.Json.Serialization;
+using Microsoft.Extensions.Options;
+using CompanyManagementSystem.API.Services.Benefits;
+using CompanyManagementSystem.API.Services.Skills;
+using CompanyManagementSystem.API.Services.Languages;
+using CompanyManagementSystem.API.Services.WorkingPositions;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using CompanyManagementSystem.API.Services.Users;
+using CompanyManagementSystem.API.Services.Roles;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -9,8 +19,9 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services.AddRouting(options => options.LowercaseUrls = true);
 
-builder.Services.AddEntityFrameworkNpgsql().AddDbContext<CompanyManagementSystem.Data.ApplicationDbContext>(opt =>
+builder.Services.AddDbContext<ApplicationDbContext>(opt =>
         opt.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection")));
 
 builder.Services.AddControllers().AddNewtonsoftJson(options =>
@@ -22,7 +33,9 @@ builder.Services.AddControllers().AddNewtonsoftJson(options =>
 
                 options.SerializerSettings.ReferenceLoopHandling = Newtonsoft.Json.ReferenceLoopHandling.Ignore;
             });
-            
+
+var MyAllowSpecificOrigins = "_myAllowSpecificOrigins";
+
 builder.Services.AddAuthentication(options =>
             {
                 options.DefaultScheme = JwtBearerDefaults.AuthenticationScheme;
@@ -31,7 +44,31 @@ builder.Services.AddAuthentication(options =>
             })
             .AddJwtBearer();
 
+builder.Services.AddCors(options =>
+                       {
+                           options.AddPolicy(name: MyAllowSpecificOrigins,
+                             policy =>
+                             {
+                                 policy
+                                 .WithOrigins("http://localhost:5173")
+                                 .AllowAnyHeader()
+                                 .AllowAnyMethod();
+                             });
+                       });
+
+builder.Services.AddScoped<IUnitOfWork, UnitOfWork>();
+
+builder.Services.AddTransient<IBenefitService, BenefitService>();
+builder.Services.AddTransient<ISkillService, SkillService>();
+builder.Services.AddTransient<IWorkingPositionService, WorkingPositionService>();
+builder.Services.AddTransient<ILanguageService, LanguageService>();
+builder.Services.AddTransient<IUserService, UserService>();
+builder.Services.AddTransient<IRoleService, RoleService>();
+
+
 var app = builder.Build();
+
+app.UseCors(MyAllowSpecificOrigins);
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
@@ -44,7 +81,7 @@ if (app.Environment.IsDevelopment())
     });
 }
 
-app.UseHttpsRedirection();
+//app.UseHttpsRedirection();
 
 app.UseAuthorization();
 
